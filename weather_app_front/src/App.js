9be7c38352e20914cf85e73611e6365f95ddc6cf
תקווה סלמון <tikvasalamon@gmail.com>
2019-10-22 17:02:30 +0300

    Creating a separate component for search and the result outside the App component so that the App component does not refresh each search

--- weather_app_front/src/App.js
+++ weather_app_front/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Weather from "./components/weather";
+import SearchAndResult from "./components/searchAndResult";
 
 class App extends Component {
   mainCities = [
@@ -17,89 +18,13 @@ class App extends Component {
       city: "Paris"
     }
   ]
-  countryInput = '';
-  cityInput = '';
-  state = {
-    country: '',
-    city: '',
-    search: false,
-    errors: ''
-  }
-
-  handleChange = event => {
-    if (event.target.name === "country") {
-      this.countryInput = event.target.value;
-    }
-    else {
-      this.cityInput = event.target.value;
-    }
-  };
-
-  getCityWeather = () => {
-    // testing if it's only english letters - 
-    if (/[^a-z]/i.test(this.countryInput) || /[^a-z]/i.test(this.cityInput)) {
-
-      this.setState({
-        errors: 'Please use English',
-        search: false
-      })
-    }
-
-    //testing if one of the fields is empty
-    else if (this.countryInput !== '' && this.cityInput !== '') {
-      this.setState({
-        country: this.countryInput,
-        city: this.cityInput,
-        search: true,
-        errors: ''
-      })
-    }
-    else {
-      // one of the fields is empty
-      this.setState({
-        errors: 'Please set both fields',
-        search: false
-      })
-    }
-  }
 
   render() {
     return (
       <div className="app-container">
-        <div className="inputs-container">
-          <div className="layover">
 
-            <div className="inputs-flex-container">
-              <div className="input-group">
-                <i className="fas fa-globe-americas"></i>
-                <input
-                  type="text"
-                  name="country"
-                  placeholder="Country..."
-                  onChange={this.handleChange}
-                />
-              </div>
-
-              <div className="input-group">
-                <i className="fas fa-city"></i>
-                <input
-                  type="text"
-                  name="city"
-                  placeholder="City..."
-                  onChange={this.handleChange}
-                />
-              </div>
-
-              <button onClick={() => this.getCityWeather()}><i class="fas fa-search"></i></button>
-            </div>
-            {this.state.errors !== '' && <div className="errors-wrapper"><p>{this.state.errors}</p></div>}
-          </div>
-        </div>
-
-        {
-          this.state.search && <Weather cardClass="search-card" country={this.state.country} city={this.state.city} />
-        }
-
+        <SearchAndResult />
+        
         <div className="flex-container">
 
           {
